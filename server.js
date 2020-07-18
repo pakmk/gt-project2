cdc098521f21f46ab9053dccdfc0d783a0e8740c
Michael Nava <M1cha3l.nava@gmail.com>
2020-07-18 18:32:12 -0400

    test

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ var bcryptjs = require("bcryptjs");
 // Setting up port and requiring models for syncing
 var PORT = process.env.PORT || 8080;
 var db = require("./models");
-
+// =========================================================================
 // Creating express app and configuring middleware needed for authentication
 var app = express();
 app.use(express.urlencoded({ extended: true }));
