37185ac8a7cb43ded51469a617fea3fddb37c62f
Michael Nava <M1cha3l.nava@gmail.com>
2020-07-18 14:23:29 -0400

    finished the media and posts.js files in models

---
+++ models/posts.js
@@ -0,0 +1,26 @@
+// Creating our Post model
+
+module.exports = function (sequelize, DataTypes) {
+  var Posts = sequelize.define("Post", {
+    // The email cannot be null, and must be a proper email before creation
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    title: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    //   type moved to media table
+    /* type: {
+            type: DataTypes.STRING,
+            allowNull: false,
+          }, */
+    body: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  });
+  return Posts;
+};
