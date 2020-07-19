ef785bc5320514f7478f141b27396aa938e0aa2e
Michael Nava <M1cha3l.nava@gmail.com>
2020-07-18 21:13:49 -0400

    added models

---
+++ models/creatives.js
@@ -0,0 +1,30 @@
+// Creating our Creative model 
+
+module.exports = function(sequelize, DataTypes) {
+  var Creatives = sequelize.define("Creative", {
+    // The email cannot be null, and must be a proper email before creation
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      //   type: { type: DataTypes.STRING }, move to media table
+      bio: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      post_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      media_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+  });
+  return Creatives;
+};
