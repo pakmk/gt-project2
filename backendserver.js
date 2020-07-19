6fd10c7e16e8a3df6c9b3237a3a96334e4ae0527
Michael Nava <M1cha3l.nava@gmail.com>
2020-07-18 22:05:24 -0400

    sucessful merge

--- backendserver.js
+++ backendserver.js
@@ -27,8 +27,15 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 // Requiring our routes
-require("./routes/html-routes.js")(app);
-require("./routes/api-routes.js")(app);
+// require("./routes/html-routes.js")(app);
+// require("./routes/api-routes.js")(app);
+const ViewsController = require("./controllers/viewsController.js");
+const APIController = require("./controllers/apiController");
+const UsersController = require("./controllers/usersController");
+// Routes
+app.use(ViewsController);
+app.use(APIController);
+app.use("/api/users", UsersController);
 
 // Syncing our database and logging a message to the user upon success
 db.sequelize.sync().then(function () {
