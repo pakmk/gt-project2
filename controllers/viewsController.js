69d0c89993897179acc0a986df0925ea187b9dc3
Gregory Monroe II <gregthemusician@gmail.com>
2020-07-20 14:56:36 -0400

    merging with master to start on front-end design

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -2,6 +2,10 @@ const express = require("express");
 const router = express.Router();
 
 
+router.get("/", (req, res) => {
+  res.render("index");
+});
+
 router.get("/index", (req, res) => {
   res.render("index");
 });
