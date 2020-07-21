54247fd8d99b47c52fd105d00d0fb1d32b15c17b
unknown <brw1820@gmail.com>
2020-07-21 11:07:13 -0400

    changes to config.json and server.js

--- server.js
+++ server.js
@@ -1,57 +1,61 @@
-// /**
-//  * REQUIRE NPM PACKAGES
-//  * REQUIRE EXTERNAL FILES
-//  */
-// const express = require("express");
-// const exphbs = require("express-handlebars");
+// Requiring necessary npm packages
+const express = require("express");
+const exphbs = require("express-handlebars");
+const session = require("express-session");
+// Requiring passport as we've configured it
+const passport = require("./config/passport");
+const bcryptjs = require("bcryptjs");
 
-// /**
-//  * DEFINE VARIABLES
-//  */
-// const PORT = process.env.PORT || 8080;
-// const app = express();
-// const db = require("./models");
-// const ViewsController = require("./controllers/viewsController.js");
+const ViewsController = require("./controllers/viewsController.js");
 // const APIController = require("./controllers/apiController");
-// const UsersController = require("./controllers/usersController");
+// const UsersController = require("./controllers/usersController")
 
-// /**
-//  * MIDDLEWARE
-//  */
-// // Parse application body as JSON
-// app.use(express.urlencoded({ extended: true }));
-// app.use(express.json());
-// // Handlebars setup
-// app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-// app.set("view engine", "handlebars");
+// Setting up port and requiring models for syncing
+const PORT = process.env.PORT || 8080;
+const app = express();
+const db = require("./models");
+// const ViewsController = require("./controllers/viewsController.js");
+const APIController = require("./controllers/apiController");
+const usersController = require("./controllers/usersController");
 
-// /**
-//  * VIEW ROUTES
-//  * API ROUTES
-//  */
-// // Serve static content for the app from the "public" directory in the application directory.
-// // app.use(express.static("public"));
-// app.use(express.static(__dirname));
+// Creating express app and configuring middleware needed for authentication
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+// Handlebars setup
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
-// // Routes
-// app.use(ViewsController);
+var router = require("./controllers/apiController.js");
+// console.log(router);
+
+// We need to use sessions to keep track of our user's login status
+app.use(
+  session({ secret: "keyboard cat", resave: true, saveUninitialized: true })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(router);
+
+// Requiring our routes
+// require("./routes/html-routes.js")(app);
+// require("./routes/api-routes.js")(app);
+
+app.use(ViewsController);
 // app.use(APIController);
 // app.use("/api/users", UsersController);
 
-// /**
-//  * DB Connection
-//  * APP LISTEN
-//  */
-// db.sequelize
-//   .sync()
-//   .then(() => {
-//     // Start our server so that it can begin listening to client requests.
-//     app.listen(PORT, function () {
-//       // Log (server-side) when our server has started
-//       console.log(`Server listening on: http://localhost:${PORT}`);
-//     });
-//   })
-//   .catch((err) => {
-//     console.log("Error connecting to the database.");
-//     console.log(err);
-//   });
+// Syncing our database and logging a message to the user upon success
+db.sequelize.sync({ force: true }).then(function () {
+  app.listen(PORT, function () {
+    console.log(
+      "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
+      PORT,
+      PORT
+    );
+  });
+})
+.catch((err) => {
+  console.log("Error connecting to the database.");
+  console.log(err);
+});
