c70d10026763fcd9d260f2ea438523612ce6f337
Gregory Monroe II <gregthemusician@gmail.com>
2020-07-21 23:17:12 -0400

    cleaned up code in server.js

--- server.js
+++ server.js
@@ -5,19 +5,16 @@ const session = require("express-session");
 // Requiring passport as we've configured it
 const passport = require("./config/passport");
 const bcryptjs = require("bcryptjs");
-// const {
-//   allowInsecurePrototypeAccess,
-// } = require("@handlebars/allow-prototype-access");
+
 const handlebars = require("handlebars");
 const ViewsController = require("./controllers/viewsController.js");
-// const APIController = require("./controllers/apiController");
-// const UsersController = require("./controllers/usersController")
+
 
 // Setting up port and requiring models for syncing
 const PORT = process.env.PORT || 8080;
 const app = express();
 const db = require("./models");
-// const ViewsController = require("./controllers/viewsController.js");
+
 const APIController = require("./controllers/apiController");
 const usersController = require("./controllers/usersController");
 
@@ -30,13 +27,11 @@ app.engine(
   "handlebars",
   exphbs({
     defaultLayout: "main",
-    // handlebars: allowInsecurePrototypeAccess(handlebars),
   })
 );
 app.set("view engine", "handlebars");
 
-var router = require("./controllers/apiController.js");
-// console.log(router);
+const router = require("./controllers/apiController.js");
 
 // We need to use sessions to keep track of our user's login status
 app.use(
@@ -46,13 +41,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 app.use(router);
 
-// Requiring our routes
-// require("./routes/html-routes.js")(app);
-// require("./routes/api-routes.js")(app);
-
 app.use(ViewsController);
-// app.use(APIController);
-// app.use("/api/users", UsersController);
 
 // Syncing our database and logging a message to the user upon success
 db.sequelize
